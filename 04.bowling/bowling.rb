73ac446497b733f7af33cc17fb348de4b87d6cf1
wata00913 <175d8639@gmail.com>
2021-08-27 22:23:52 +0900

    リファクタリング：関数名の変更とフレームに関するメソッドと変数をクラスにまとめる

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,28 +1,38 @@
 N = 10
 STRIKE = 'X'
 
-def number_of_down_pins(frame, i_throw = 0)
-  # ストライクの場合はi_throwの引数に限らず要素数が1
-  f = frame[0..(i_throw - 1)]
-  f.inject(0) { |num, score| score == STRIKE ? num + 10 : num + score }
+class Frame
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def strike?
+    @scores.include?(STRIKE)
+  end
+
+  def spare?
+    @scores.sum == 10
+  end
+
+  def number_of_down_pins(i_throw = 0)
+    # ストライクの場合はi_throwの引数に限らず要素数が1
+    sub_scores = @scores[0..(i_throw - 1)]
+    sub_scores.inject(0) { |num, score| score == STRIKE ? num + 10 : num + score }
+  end
 end
 
-def strike?(frame)
-  frame.include?(STRIKE)
-end
 
-def spare?(frame)
-  frame.sum == 10
-end
-
-def score(frames, to)
+def frame_score(frames, to)
   # offset指定をしたいので、mapでEnumeratorに変換する
   (1..to).map.with_index(0).inject(0) do |score, (i_th_frame, idx)|
+    current_frame = Frame.new(frames[idx])
     added_score = if i_th_frame == 10
                     last_frame_score(frames[idx])
-                  elsif strike?(frames[idx])
+                  elsif current_frame.strike?
                     strike_score(frames, i_th_frame + 1, idx + 1)
-                  elsif spare?(frames[idx])
+                  elsif current_frame.spare?
                     spare_score(frames[idx + 1])
                   else
                     normal_score(frames[idx])
@@ -36,23 +46,27 @@ def normal_score(frame)
 end
 
 def spare_score(next_frame)
-  10 + number_of_down_pins(next_frame, 1)
+  frame = Frame.new(next_frame)
+  10 + frame.number_of_down_pins(1)
 end
 
 def strike_score(frames, next_i_th_frame, next_idx)
-  if strike?(frames[next_idx]) && next_i_th_frame < 10
-    10 + 10 + number_of_down_pins(frames[next_idx + 1], 1)
+  next_frame = Frame.new(frames[next_idx])
+  if next_frame.strike? && next_i_th_frame < 10
+    after_next_frame = Frame.new(frames[next_idx + 1])
+    10 + 10 + after_next_frame.number_of_down_pins(1)
   else
     # 次の投球がストライク以外または次のフレームが10フレーム目の場合
-    10 + number_of_down_pins(frames[next_idx], 2)
+    10 + next_frame.number_of_down_pins(2)
   end
 end
 
 def last_frame_score(last_frame)
-  number_of_down_pins(last_frame)
+  last_frame = Frame.new(last_frame)
+  last_frame.number_of_down_pins
 end
 
-def calc_score
+def game_score
   input = ARGV[0]
   scores = input.split(',').map do |i|
     if i == STRIKE
@@ -62,7 +76,8 @@ def calc_score
     end
   end
   frames = to_frames(scores)
-  puts score(frames, 10)
+  # 10フレーム目のスコアを求める
+  puts frame_score(frames, 10)
 end
 
 def to_frames(scores)
@@ -83,4 +98,4 @@ def to_frames(scores)
   frames
 end
 
-calc_score if $0 == __FILE__
+game_score if $0 == __FILE__
--- 04.bowling/bowling_test.rb
+++ 04.bowling/bowling_test.rb
@@ -22,36 +22,36 @@ class BowlingTest < Minitest::Test
 
   def test_1フレーム目のスコア
     input = [[1, 2]]
-    assert_equal 3, score(input, 1)
+    assert_equal 3, frame_score(input, 1)
   end
 
   def test_2フレーム目のスコア
     input = [[1, 2], [3, 5]]
-    assert_equal 11, score(input, 2)
+    assert_equal 11, frame_score(input, 2)
   end
 
   def test_スペアを含んだスコア
     input = [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3]]
-    assert_equal 38, score(input, 4)
+    assert_equal 38, frame_score(input, 4)
   end
 
   def test_スペアの次の投球がストライク文字列になる場合のスコア
     input = [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], ['X'], [9, 1]]
-    assert_equal 78, score(input, 6)
+    assert_equal 78, frame_score(input, 6)
   end
 
   def test_10フレームまでのスコア
     input = [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], ['X'], [9, 1], [8, 0], ['X'], [6, 3]]
-    assert_equal 132, score(input, 10)
+    assert_equal 132, frame_score(input, 10)
   end
 
   def test_10フレームまでのスコアでかつ10フレーム目にストライクを含む
     input = [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], ['X'], [9, 1], [8, 0], ['X'], ['X', 6, 4]]
-    assert_equal 150, score(input, 10)
+    assert_equal 150, frame_score(input, 10)
   end
 
   def test_全てストライクの場合のスコア
     input = [['X'], ['X'], ['X'], ['X'], ['X'], ['X'], ['X'], ['X'], ['X'], %w[X X X]]
-    assert_equal 300, score(input, 10)
+    assert_equal 300, frame_score(input, 10)
   end
 end
