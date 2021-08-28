39c5747e43e885cf77cbbe67e5a58b2fb78af418
wata00913 <175d8639@gmail.com>
2021-08-28 22:47:16 +0900

    Bowlingクラスを作成

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -23,82 +23,99 @@ class Frame
   end
 end
 
+class Bowling
+  attr_reader :frames
+
+  def initialize(frames)
+    @frames = frames
+  end
+
+  def frame_score(to)
+    # offset指定をしたいので、mapでEnumeratorに変換する
+    (1..to).map.with_index(0).inject(0) do |score, (ith_throw, idx)|
+      current_frame = get_frame(idx)
+      next_frame = get_frame(idx + 1)
+      added_score = if ith_throw == 10
+                      last_frame_score(current_frame)
+                    elsif current_frame.strike?
+                      strike_score(ith_throw + 1, idx + 1)
+                    elsif current_frame.spare?
+                      spare_score(next_frame)
+                    else
+                      normal_score(current_frame)
+                    end
+      score + added_score
+    end
+  end
+
+  private
+
+  def normal_score(frame)
+    frame.number_of_down_pins
+  end
+
+  def spare_score(next_frame)
+    10 + next_frame.number_of_down_pins(1)
+  end
+
+  def strike_score(next_ith_frame, next_idx)
+    next_frame = get_frame(next_idx)
+    if next_frame.strike? && next_ith_frame < 10
+      after_next_frame = get_frame(next_idx + 1)
+      10 + 10 + after_next_frame.number_of_down_pins(1)
+    else
+      # 次の投球がストライク以外または次のフレームが10フレーム目の場合
+      10 + next_frame.number_of_down_pins(2)
+    end
+  end
+
+  def last_frame_score(last_frame)
+    last_frame.number_of_down_pins
+  end
+
+  def get_frame(idx)
+    @frames[idx]
+  end
+end
 
 def frame_score(frames, to)
-  # offset指定をしたいので、mapでEnumeratorに変換する
-  (1..to).map.with_index(0).inject(0) do |score, (ith_throw, idx)|
-    current_frame = get_frqme(frames, idx)
-    next_frame = get_frqme(frames, idx + 1)
-    added_score = if ith_throw == 10
-                    last_frame_score(current_frame)
-                  elsif current_frame.strike?
-                    strike_score(frames, ith_throw + 1, idx + 1)
-                  elsif current_frame.spare?
-                    spare_score(next_frame)
-                  else
-                    normal_score(current_frame)
-                  end
-    score + added_score
-  end
+  xx_frames = get_xx_frames(frames)
+  bowling = Bowling.new(xx_frames)
+  bowling.frame_score(to)
 end
 
-# frameをカプセル化
-# 後でframesの要素をFrameインスタンスにするため
-def get_frqme(frames, idx)
-  Frame.new(frames[idx])
-end
-
-def normal_score(frame)
-  frame.number_of_down_pins
-end
-
-def spare_score(next_frame)
-  10 + next_frame.number_of_down_pins(1)
-end
-
-def strike_score(frames, next_ith_frame, next_idx)
-  next_frame = get_frqme(frames, next_idx)
-  if next_frame.strike? && next_ith_frame < 10
-    after_next_frame = get_frqme(frames, next_idx + 1)
-    10 + 10 + after_next_frame.number_of_down_pins(1)
-  else
-    # 次の投球がストライク以外または次のフレームが10フレーム目の場合
-    10 + next_frame.number_of_down_pins(2)
-  end
-end
-
-def last_frame_score(last_frame)
-  last_frame.number_of_down_pins
+def get_xx_frames(frames)
+  frames.map { |el| Frame.new(el) }
 end
 
 def game_score
   input = ARGV[0]
-  scores = input.split(',').map do |i|
+  all_scores = input.split(',').map do |i|
     if i == STRIKE
       i
     else
       i.to_i
     end
   end
-  frames = to_frames(scores)
+  frames = to_frames(all_scores)
   # 10フレーム目のスコアを求める
   puts frame_score(frames, 10)
 end
 
-def to_frames(scores)
+def to_frames(all_scores)
   frames = []
   nth_throw = 1
   (1..N).each do |i|
-    frame = []
+    scores = []
     loop do
-      score = scores[nth_throw - 1]
-      frame << score
+      score = all_scores[nth_throw - 1]
+      scores << score
       nth_throw += 1
-      break if i < 10 && (score == STRIKE || frame.length == 2)
+      break if i < 10 && (score == STRIKE || scores.length == 2)
 
-      break if i == 10 && nth_throw > scores.length
+      break if i == 10 && nth_throw > all_scores.length
     end
-    frames << frame
+    frames << scores
   end
   frames
 end
