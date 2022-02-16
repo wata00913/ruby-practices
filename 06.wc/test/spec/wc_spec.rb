71945ac7ec53923b785577f2ca22952cd6f62621
wata00913 <175d8639@gmail.com>
2022-02-16 21:07:02 +0900

    残りのテストコードを追加

--- 06.wc/test/spec/wc_spec.rb
+++ 06.wc/test/spec/wc_spec.rb
@@ -30,3 +30,97 @@ RSpec.describe 'WordsCounter' do
     end
   end
 end
+
+RSpec.describe 'LinesCounter' do
+  describe 'count_lines' do
+    context '文字列がある場合' do
+      it '改行文字をカウントする' do
+        str = "hoge\nf\n"
+        expect(count_lines(str))
+      end
+    end
+
+    context '空文字列の場合' do
+      it '改行文字をカウントする' do
+        str = ''
+        expect(count_lines(str))
+      end
+    end
+  end
+end
+
+RSpec.describe 'CharctersCounter' do
+  describe 'count_chars' do
+    context '文字列にマルチバイト文字を含む場合' do
+      it '文字列のバイト長をカウントする' do
+        str = "あいう😄\n"
+        expect(count_chars(str)).to eq 14
+      end
+    end
+    context '文字列にマルチバイト文字を含まない場合' do
+      it '文字列のバイト長をカウントする' do
+        str = "hoge\n"
+        expect(count_chars(str)).to eq 5
+      end
+    end
+  end
+end
+
+RSpec.describe 'WCModel' do
+  describe 'create_counter' do
+    context '入力がファイルの場合' do
+      it 'カウンターを作成する' do
+        input_path = './input.txt'
+        File.open(input_path) do |f|
+          expect(create_counter(f)).to eq({ lines: 3, words: 4, chars: 26 })
+        end
+      end
+    end
+
+    context '入力が標準入力の場合' do
+      it 'カウンターを作成する' do
+        StringIO.open("😄😄\n") do |io|
+          expect(create_counter(io)).to eq({ lines: 1, words: 1, chars: 9 })
+        end
+      end
+    end
+  end
+
+  describe 'total_count' do
+    it '複数カウンターの合計を求める' do
+      counters = [
+        { lines: 3, words: 4, chars: 26 },
+        { lines: 86, words: 223, chars: 2470 }
+      ]
+      expect(total_count(counters)).to eq({ lines: 89, words: 227, chars: 2496 })
+    end
+  end
+end
+
+RSpec.describe 'ViewWC' do
+  describe 'displayed_wc_line' do
+    context '入力がファイルかつ全てのオプションを指定する場合' do
+      it '単語数、行数、文字数、ファイル名を連結した文字列を返す' do
+        counter = { lines: 3, words: 4, chars: 26 }
+        file_name = 'input.txt'
+        expect(displayed_wc_line(counter, file_name)).to eq '       3       4      26 input.txt'
+      end
+    end
+
+    context '入力がファイルかつ単語数オプションのみを指定する場合' do
+      it '単語数、ファイル名を連結した文字列返す' do
+        counter = { lines: 3, words: 4, chars: 26 }
+        file_name = 'input.txt'
+        expect(displayed_wc_line(counter, file_name, visible_words: false, visible_chars: false)).to eq '       3 input.txt'
+      end
+    end
+
+    context '入力が標準入力かつ全てのオプションを指定する場合' do
+      it '単語数、行数、文字数を連結した文字列返す' do
+        counter = { lines: 3, words: 4, chars: 26 }
+        file_name = ''
+        expect(displayed_wc_line(counter, file_name)).to eq '       3       4      26'
+      end
+    end
+  end
+end
