48c9f6fc3738b2fcd4353b653a2e995011a344fa
wata00913 <175d8639@gmail.com>
2022-10-17 16:23:14 +0900

    Bowlingモジュールに定数と特異メソッドの追加とそれに伴う修正

--- 07.bowling_object/lib/bowling.rb
+++ 07.bowling_object/lib/bowling.rb
@@ -5,9 +5,12 @@ $LOAD_PATH.unshift(__dir__)
 module Bowling
   MAX_PINS = 10
   MAX_FRAME = 10
-end
+  STRIKE_CHAR = 'X'
 
-STRIKE_CHAR = 'X'
+  def self.convert_to_i(str_num_or_char)
+    str_num_or_char == STRIKE_CHAR ? MAX_PINS : str_num_or_char.to_i
+  end
+end
 
 autoload :Game, 'bowling/game'
 autoload :Frame, 'bowling/frame'
@@ -20,13 +23,7 @@ def print_game_score
 end
 
 def parse(input)
-  input.split(',').map do |c|
-    if c == STRIKE_CHAR
-      Bowling::MAX_PINS
-    else
-      c.to_i
-    end
-  end
+  input.split(',').map { |s| Bowling.convert_to_i(s) }
 end
 
 print_game_score if $PROGRAM_NAME == __FILE__
