c176a1beec5eb2a1313f26d15d1d6156f4e45f0c
wata00913 <175d8639@gmail.com>
2022-10-25 11:43:08 +0900

    lsコマンド処理を追加

--- 08.ls_object/lib/ls.rb
+++ 08.ls_object/lib/ls.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'optparse'
+
 $LOAD_PATH.unshift(__dir__)
 
 module Ls
@@ -8,3 +10,23 @@ module Ls
   autoload :ShortFormatter, 'ls/short_formatter'
   autoload :LongFormatter, 'ls/long_formatter'
 end
+
+def parse_options
+  opts = { dot: false, reverse: false, long: false, col: 3 }
+  OptionParser.new do |opt_parser|
+    opt_parser.on('-a') { |v| opts[:dot] = v }
+    opt_parser.on('-l') { |v| opts[:long] = v }
+    opt_parser.on('-r') { |v| opts[:reverse] = v }
+  end.parse!(ARGV)
+  opts[:paths] = ARGV
+  opts
+end
+
+def ls
+  opts = parse_options
+  formatter = opts[:long] ? Ls::LongFormatter.new(opts) : Ls::ShortFormatter.new(opts)
+
+  puts formatter.to_lines.join("\n")
+end
+
+ls if __FILE__ == $PROGRAM_NAME
